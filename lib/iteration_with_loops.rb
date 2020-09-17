5402d879e8f3945f11961482051f073fe9e4b1bc
Sam Passarelli <passarelli.sam@gmail.com>
2020-09-17 01:53:21 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,7 @@ def join_nested_strings(src)
   while row_index < src.count do
     element_index = 0 
     while element_index < src[row_index].count do
-      if element_index.class
+      if element_index.class == 
     end
   end
 end
